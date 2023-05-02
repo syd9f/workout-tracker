ae05e4762b5a4c7cfae814388a6fbe601d872153
sydnief <sydniefarrell@gmail.com>
2023-05-01 21:30:35 -0400

    installed connect-session-sequelize

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,22 @@
 // get reqs
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+    try {
+      // Get all users, sorted by name
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+        order: [['name', 'ASC']],
+      });
+  
+      // Serialize user data so templates can read it
+      const users = userData.map((project) => project.get({ plain: true }));
+  
+      // Pass serialized data into Handlebars.js template
+      res.render('homepage', { users });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
