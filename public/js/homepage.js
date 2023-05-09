f307b46db22d9ef079ffc44965690627c3b6296f
sydnief <sydniefarrell@gmail.com>
2023-05-09 19:21:31 -0400

    worked on getting selected button value to display in scheduled exercises

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,12 +1,8 @@
-// event listener on schedule button to load the schedule.handlebars
-const scheduleButton = document.getElementById('scheduleButton');
+var selected_exercise = document.getElementsByClassName('exercise-listbtn')
+console.log(selected_exercise)
+var exercise_title = document.getElementById('exercise-title')
+console.log(exercise_title)
 
-function scheduleExercise() {
-  const exerciseDate = prompt('Enter a Date')
-  alert(exerciseDate);
-}
-scheduleButton.addEventListener("click", scheduleExercise);
-
-// event listener on "mark complete" button on scheduled exercises to add 1 to the Exercises Completed section of Accomplishments
-
-// event listener on "mark complete" button on goals to add 1 to the Goals Completed section of Accomplishments
+$("modal-SaveBtn").click(function() {
+  exercise_title.append(selected_exercise.val);
+});
