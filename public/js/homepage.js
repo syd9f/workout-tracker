3040856a30bfb0272cf78b4d30d3ea4e5e1369b8
sydnief <sydniefarrell@gmail.com>
2023-05-08 22:16:53 -0400

    fixed api

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,23 +1,12 @@
-const newFormHandler = async (event) => {
-    event.preventDefault();
+// event listener on schedule button to load the schedule.handlebars
+const scheduleButton = document.getElementById('scheduleButton');
 
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
-  
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
-        method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+function scheduleExercise() {
+  const exerciseDate = prompt('Enter a Date')
+  alert(exerciseDate);
+}
+scheduleButton.addEventListener("click", scheduleExercise);
 
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create project');
-      }
-    }
-  };
+// event listener on "mark complete" button on scheduled exercises to add 1 to the Exercises Completed section of Accomplishments
+
+// event listener on "mark complete" button on goals to add 1 to the Goals Completed section of Accomplishments
