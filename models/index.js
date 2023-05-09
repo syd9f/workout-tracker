96a7c329b63a21bddeb785d276e8684a48b4747d
stevore <skamau22@wustl.edu>
2023-05-08 17:00:15 -0700

    made changes to models/index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
 const User = require('./User');
 
 module.exports = { User };
+
+User.hasMany(Workouts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Workouts.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Workouts };
