b9f81b785848063052a57e2bdeec56ddbbe8b6ea
sydnief <sydniefarrell@gmail.com>
2023-05-09 21:20:24 -0400

    added an id to schedule

--- models/Schedule.js
+++ models/Schedule.js
@@ -5,6 +5,12 @@ class Schedule extends Model {}
 
 Schedule.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
